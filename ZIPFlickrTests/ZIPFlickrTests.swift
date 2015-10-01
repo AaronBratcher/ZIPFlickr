05da6c2ca85d3566f797a61d20b1eccdc6c07117
Aaron Bratcher <aaronbratcher1@gmail.com>
2015-10-01 18:53:02 -0400

    Initial Commit

---
+++ ZIPFlickrTests/ZIPFlickrTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZIPFlickrTests.swift
+//  ZIPFlickrTests
+//
+//  Created by Aaron Bratcher on 10/1/15.
+//  Copyright © 2015 Aaron Bratcher. All rights reserved.
+//
+
+import XCTest
+@testable import ZIPFlickr
+
+class ZIPFlickrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
